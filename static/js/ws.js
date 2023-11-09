9450cd50140c2a352a2cc2e72e3b3b88799e7150
Damian Wilkolek <dwilkolek@protonmail.com>
2023-11-09 02:02:57 +0100

    fix: flickering hover

--- static/js/ws.js
+++ static/js/ws.js
@@ -6,8 +6,7 @@ function createQuerySelector(element) {
   const path = [];
   let currentElement = element;
   let error = false;
-
-  while (currentElement.tagName !== "BODY") {
+  while (currentElement.tagName.toLocaleLowerCase() !== "body") {
     const parent = currentElement.parentElement;
 
     if (!parent) {
@@ -62,77 +61,74 @@ const blankPosition = JSON.stringify({
 
 let sock = null;
 const referenceBoxes = ["section"];
-let lastPosition = blankPosition;
+let toSend = blankPosition;
+let lastSend = blankPosition;
+setInterval(() => {
+  const msg = toSend
+  if (sock && toSend != lastSend) {
+    console.log("Sending", blankPosition == msg)
+    lastSend = toSend
+    sock.send(msg)
+    return
+  } else {
+    console.log('NOOP')
+  }
+}, 16);
+
 addEventListener("pointermove", function (ev) {
   if (sock) {
-    const hovered = document.querySelectorAll(":hover");
-    let selectedEl = hovered[hovered.length - 1];
-    let valid = false;
+    const hovered = document.querySelectorAll(":hover:not(.cursor)");
+    let selectedEl;
     for (let i = hovered.length - 1; i > 0; i--) {
-      const el = hovered[i];
-      if (referenceBoxes.includes(el.nodeName.toLocaleLowerCase())) {
-        selectedEl = el;
-        valid = true;
+      const tmpEl = hovered[i];
+      if (referenceBoxes.includes(tmpEl.nodeName.toLocaleLowerCase())) {
+        selectedEl = tmpEl;
         break;
       }
     }
-    // if (!valid && lastPosition == blankPosition) {
-    //   return
-    // }
 
     let nextPosition = blankPosition;
-    if (valid) {
+    if (selectedEl) {
       const rect = selectedEl.getBoundingClientRect();
       const querySelector = createQuerySelector(selectedEl);
 
       if (querySelector) {
         nextPosition = JSON.stringify({
-          elementQuery: createQuerySelector(selectedEl),
+          elementQuery: querySelector,
           x: (ev.clientX - rect.left) / rect.width,
           y: (ev.clientY - rect.top) / rect.height,
         });
       }
     }
-    
-    if (nextPosition != lastPosition) {
-      lastPosition = nextPosition;
-      sock.send(lastPosition);
-    }
+    toSend = nextPosition;
   }
 });
 
 async function createSession() {
-  const session = await apiCreateSession()
-  joinSession(session)
+  const session = await apiCreateSession();
+  joinSession(session);
 }
-
+const cursors = []
 function joinSession(session) {
   sock = new WebSocket(
     `${window.location.origin.replace("http", "ws")}/ws/${session.id}/cursors`
   );
   sock.onmessage = (event) => {
     const positions = JSON.parse(event.data ?? "[]");
-    let notHandled = [...positions];
-    document.querySelectorAll(".cursor")?.forEach((c) => {
-      newPosition = positions.find((p) => p.clientId == c.dataset.clientId);
-      if (newPosition) {
-        notHandled = notHandled.filter(
-          (n) => n.clientId !== newPosition.clientId
-        );
-        updateNodePosition(newPosition, c);
-        c.style.top = newPosition.top;
+    for (const cursorPosition of positions) {
+      let cursorEl = cursors[cursorPosition.clientId]
+      if (!cursorEl) {
+        const cursorEl = document.createElement("div");
+      
+        cursorEl.dataset.clientId = cursorPosition.clientId;
+        cursorEl.classList.add("cursor", `cursor-${cursorPosition.clientId}`);
+        updateNodePosition(cursorPosition, cursorEl);
+        document.body.appendChild(cursorEl);
+        cursors[cursorPosition.clientId] = cursorEl
       } else {
-        c.remove();
+        updateNodePosition(cursorPosition, cursorEl);
       }
-    });
-    for (const cursorPosition of notHandled) {
-      const cursorEl = document.createElement("div");
-      cursorEl.dataset.clientId = cursorPosition.clientId;
-
-      cursorEl.classList.add("cursor");
-
-      updateNodePosition(cursorPosition, cursorEl);
-      document.body.appendChild(cursorEl);
+      
     }
   };
 }
@@ -140,10 +136,6 @@ function joinSession(session) {
 function updateNodePosition(cursorPosition, element) {
   const boxElement = document.querySelector(cursorPosition.elementQuery);
   const boxElementRect = boxElement.getBoundingClientRect();
-
-  const cursorEl = document.createElement("div");
-
-  cursorEl.classList.add("cursor");
   element.style.left = `${Math.round(
     boxElementRect.width * cursorPosition.x +
       boxElementRect.left +
